2e5acc2bec88a033c8abe0e08146ec117a7c190a
Christian <christiancook52@gmail.com>
2021-03-07 17:00:31 -0500

    Added demo files to repo

---
+++ Unity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;        // object to follow
+    public float smoothingSpeed;    // "lag" between player moving and camera catching up
+
+    public Vector3 offset;          // offset vector from player
+
+    private void Start() {
+        // initialize camera position and rotation
+        transform.position = target.transform.position + offset;
+    }
+
+    private void Update() {
+        // move camera with player, interpolating based on amount of smoothing
+        Vector3 newPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothingSpeed);
+    }
+}
