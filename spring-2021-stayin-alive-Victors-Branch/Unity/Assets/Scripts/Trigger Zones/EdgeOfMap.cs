9a075ba226db28ef999f151cf7342d2d39ed848d
Victor G <vgainessp@gmail.com>
2021-03-29 00:32:02 -0400

    Creates the First level, also changed stuff for the camera control

---
+++ spring-2021-stayin-alive-Victors-Branch/Unity/Assets/Scripts/Trigger Zones/EdgeOfMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour
+{
+	// temporary, deleet after demo
+	public GameObject zombieGroup;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.CompareTag("Player")) {
+			zombieGroup.SetActive(true);
+		}
+	}
+}
