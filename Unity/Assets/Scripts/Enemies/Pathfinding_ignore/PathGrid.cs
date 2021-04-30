5b7cc1e04863dcb43037d568ce6fb428db9de329
Pendragon64 <78666180+Pendragon64@users.noreply.github.com>
2021-04-30 02:04:47 -0400

    Enemy basic ai
    I tried to do a fancy pathfinding thing but that imploded, at least I learned stuff from it

---
+++ Unity/Assets/Scripts/Enemies/Pathfinding_ignore/PathGrid.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGrid<TGridObject>
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector2 originPosition;
+
+    private TGridObject[,] gridArray;
+
+    public PathGrid(int width, int height, float cellSize, Vector2 originPosition, Func<PathGrid<TGridObject>, int, int, TGridObject> createGridObject){
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+    
+
+        gridArray = new TGridObject[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++){
+            for (int y = 0; y< gridArray.GetLength(1); y++){
+                gridArray[x,y] = createGridObject(this, x, y);
+            }
+        }
+    }
+
+    public Vector2 GetWorldPosition(int x, int y){
+        return new Vector2(x,y) * cellSize + originPosition;
+    }
+
+    public void GetXY(Vector2 worldPosition, out int x, out int y){
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value){
+        if (x >= 0 && y >= 0 && x < width && y < height){
+            gridArray[x,y] = value;
+        } 
+    }
+
+    public void SetGridObject(Vector2 worldPosition, TGridObject value){
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetGridObject(x, y, value);
+    }
+
+    public TGridObject GetGridObject(int x, int y){
+        if (x >= 0 && y >= 0 && x < width && y < height){
+            return gridArray[x,y];
+        } 
+        return default(TGridObject);
+    }
+
+    public TGridObject GetGridObject(Vector2 worldPosition){
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetGridObject(x,y);
+    }
+
+    public float GetCellSize(){
+        return cellSize;
+    }
+
+    public int GetWidth(){
+        return width;
+    }
+
+    public int GetHeight(){
+        return height;
+    }
+
+}
---
+++ Unity/Assets/Scripts/Enemies/Pathfinding_ignore/PathNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    public PathGrid<PathNode> grid;
+    public int x;
+    public int y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public bool isWalkable;
+    public PathNode cameFromNode;
+
+    public PathNode(PathGrid<PathNode> grid, int x, int y){
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true; //gonna have to manipulate this with collision stuff to get the true thing
+    }
+
+    public void CalculateFCost(){
+        fCost = hCost + gCost;
+    }
+
+    public override string ToString() {
+        return x + "," + y;
+    }
+}
